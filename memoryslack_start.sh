5def6f534d026db616e1d1e3ed2ff29e50a53e98
arjunsq <arjunsoman@qburst.com>
2018-07-18 10:53:35 +0530

    Removed loop in shell script

--- memoryslack_start.sh
+++ memoryslack_start.sh
@@ -2,21 +2,20 @@
 
 source /home/user/Documents/projects/memoryslack/config
 
-while true;
-do
+y=$( free -m |grep Mem: | awk '{print $2}')
 
-free -m |grep Mem: > /home/user/Documents/projects/memoryslack/free.txt
-awk '{print $7}' /home/user/Documents/projects/memoryslack/free.txt > /home/user/Documents/projects/memoryslack/free1.txt
-x=$(cat /home/user/Documents/projects/memoryslack/free1.txt)
+x=$( free -m |grep Mem: | awk '{print $7}')
 
-echo " $x : $limit : $webhook "
-usage=$[100-$[ $[$x *100]/ 7869 ]]
+
+echo " $x : $limit : $y :$webhook   "
+usage=$[100-$[ $[$x *100]/ $y ]]
 echo usage=$usage %
 echo '{"text":"Attention there is a memory spike to a percentage of '$usage'% !"}' > /home/user/Documents/projects/memoryslack/message.txt
+
+
 if [ $x -lt $limit ]
 then
     echo "memory spike "
     curl -X POST -H 'Content-type: application/json' -d @/home/user/Documents/projects/memoryslack/message.txt $webhook
 fi
-sleep 10
-done
+
