b6102891444e94471e3198ac51c6c217427354a0
arjunsq <arjunsoman@qburst.com>
2018-07-26 14:00:40 +0530

    final

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# memoryslack
+# Memoryslack
 
 System service that alerts memory usage spikes on slack.
--- memoryslack_start.sh
+++ memoryslack_start.sh
@@ -2,20 +2,18 @@
 
 source /home/user/Documents/projects/memoryslack/config
 
-y=$( free -m |grep Mem: | awk '{print $2}')
+y=$( free -m | grep Mem: | awk '{print $2}')
 
-x=$( free -m |grep Mem: | awk '{print $7}')
+x=$( free -m | grep Mem: | awk '{print $7}')
 
 
 echo " $x : $limit : $y :$webhook   "
 usage=$[100-$[ $[$x *100]/ $y ]]
 echo usage=$usage %
-echo '{"text":"Attention there is a memory spike to a percentage of '$usage'% !"}' > /home/user/Documents/projects/memoryslack/message.txt
-
 
 if [ $x -lt $limit ]
 then
-    echo "memory spike "
-    curl -X POST -H 'Content-type: application/json' -d @/home/user/Documents/projects/memoryslack/message.txt $webhook
+    echo "memory spike"
+    curl -X POST -H 'Content-type: application/json' -d '{"text":"Attention there is a memory spike to a percentage of '$(echo $usage)'% !"}' $webhook
 fi
 
